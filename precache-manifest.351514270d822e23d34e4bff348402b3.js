644e4cdb5ef86420733dba74dc596fb4a2184564
ghackg11 <gautambehal20@gmail.com>
2021-12-22 20:51:26 +0530

    Updates

---
+++ precache-manifest.351514270d822e23d34e4bff348402b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a10ab138992092f278d",
+    "url": "/Code-Indentator/static/js/main.1a10ab13.chunk.js"
+  },
+  {
+    "revision": "ce05c2d905fadebb499c",
+    "url": "/Code-Indentator/static/js/1.ce05c2d9.chunk.js"
+  },
+  {
+    "revision": "80dcc332b5643b5cc61a",
+    "url": "/Code-Indentator/static/js/runtime~main.80dcc332.js"
+  },
+  {
+    "revision": "edef7f61f6b428060fb5ece85865d157",
+    "url": "/Code-Indentator/index.html"
+  }
+];
